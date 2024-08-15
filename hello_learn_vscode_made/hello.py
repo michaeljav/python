b32ed528bb19b5f699af4476dd2fc8904eb7a7de
Michael Javier <m.mota@concentra.com.do>
2024-08-15 13:48:44 -0400

    created code to test

---
+++ hello_learn_vscode_made/hello.py
@@ -0,0 +1,11 @@
+# msg="Roll a dice"
+# print(msg)
+
+
+print("Hello,!",__name__)
+def say_hello():
+    print("Hello, World!",__name__)
+    print("Hello, World!",__file__)
+
+if __name__ == "__main__":
+    say_hello()
---
+++ hello_without_vscode/src/main.py
@@ -0,0 +1,5 @@
+def main():
+  print("Hello Michael")
+
+if __name__ == "__main__":
+  main()
